1d423397644b2a9311a140a1c7c2ab7de539851e
tfrangiem <tfrangiem@gmail.com>
2016-07-01 18:29:45 -0400

    Exercise_2.5

---
+++ action_page.php
@@ -0,0 +1,45 @@
+<!--Exercise 2.5: Explore HTML5 Forms and Input Type
+action_page.php
+-->
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+	<title>Submission received</title>
+
+</head>
+
+<body>
+
+	<table>
+
+	<tr>
+		<th><h1 align="center">Thank You!</h1></th>
+	</tr>
+	
+		<g font-size="20" font-family="sans-serif" fill="black" stroke="none" text-anchor="middle" align="center">
+	
+	<tr>		
+		<td><h4 align="center">Your form has been successfully submitted!</h4></td>
+	</tr>
+	<tr>
+		<td><h4 align="center">Check your email account, we have sent you a copy of your order</h4></td>
+	</tr>
+	<tr>
+		<td><h3 align="center">We are looking forward to meet your car! :)</h3></td>
+	</tr>
+	<tr>	
+		<td><img height="200" src="bluecar.png" alt="$$$CarWash" ></td>
+	</tr>
+	<tr>
+		<td><h6 align="center">Answer the survey to receive a gift when you bring your car.<br> Hint!: is at the very bottom...</h6></td>
+	</tr>
+	
+</table>
+
+</body>
+
+</html>
